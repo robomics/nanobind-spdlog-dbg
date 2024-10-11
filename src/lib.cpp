f4d895c785cd076806e534bef977b17ccbe1ec8c
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-10-11 16:50:00 +0200

    Initial commit

---
+++ src/lib.cpp
@@ -0,0 +1,15 @@
+// Copyright (C) 2024 Roberto Rossini <roberros@uio.no>
+//
+// SPDX-License-Identifier: MIT
+
+#include <nanobind/nanobind.h>
+#include <spdlog/spdlog.h>
+
+static void do_log(std::string_view msg) { spdlog::info(msg); }
+
+namespace nb = nanobind;
+NB_MODULE(_nanobind_spdlog_dbg, m) {
+  m.doc() = "FOO";
+
+  m.def("do_log", &do_log, nb::arg("message") = "do_log() called");
+}
---
+++ src/nanobind_spdlog_dbg/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2024 Roberto Rossini <roberros@uio.no>
+#
+# SPDX-License-Identifier: MIT
+
+from importlib.metadata import version
+
+from ._nanobind_spdlog_dbg import do_log
+
+__version__ = version("nanobind_spdlog_dbg")
